5fe84851c545a5cd622c6d7ce9c0a0e38021f52f
Aviv Elbag <aviv_elbag@umail.ucsb.edu>
2018-07-01 23:47:53 -0700

    Added Jennur as user, added notebook

--- Geography_Notebooks/admin.py
+++ Geography_Notebooks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from graph.models import Graph
+from Geography_Notebooks.models import Notebook
 
+admin.site.register(Notebook)
+admin.site.register(Graph)
 # Register your models here.
