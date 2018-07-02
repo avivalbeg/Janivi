4340e1568f873505422d1783152073dae007592e
jennur <jenny.bonsak@gmail.com>
2018-07-02 08:54:23 +0200

    Not mine

--- Geography_Notebooks/views.py
+++ Geography_Notebooks/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Notebook
 
 def index(request):
-    return render(request,template_name="index.html")
+    Notebooks = Notebook.objects.all()
+    context = {
+        "Notebooks":Notebooks
+    }
+    return render(request,template_name="index.html",context=context)
 
 def detail(request):
     return render(request,template)
